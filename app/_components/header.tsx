474be78062ff96e57477c2570805d0b86dfdaa0a
gabrielNunesKlein <gabrielgnk@outlook.com>
2024-05-04 15:44:39 -0300

    Style: logo in header, Feat: link in logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,11 +2,15 @@ import Image from 'next/image'
 import React from 'react'
 import { Button } from './ui/button'
 import { MenuIcon } from 'lucide-react'
+import Link from 'next/link'
 
 export const Header = () => {
   return (
     <div className='flex justify-between pt-6 px-3'>
-        <Image src="/logo.png" height={30} width={100} alt='FSW FOODS' />
+
+        <Link href="/">
+          <Image className='object-contain' src="/logo.png" height={30} width={100} alt='FSW FOODS' />
+        </Link>
 
         <Button
             size="icon"
